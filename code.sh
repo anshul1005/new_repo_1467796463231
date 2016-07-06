1cc858955af3d0f074a3ef636974fdd29be10728
Anshul Garg <QAIT\anshulgarg@asd1578po02037.qait.com>
2016-07-06 14:44:29 +0530

    Initial commit

---
+++ code.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd
+git clone git@github.com:anshul1005/new_repo_1467796463231.git
+cp /home/anshulgarg/workspace/github/code.sh new_repo_1467796463231
+cd new_repo_1467796463231/
+git add -A
+git commit -m "Initial commit"
+git push origin master
